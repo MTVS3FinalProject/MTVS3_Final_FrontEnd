33ca5aca61bfbcf5d31033e1e31e1f71c1f5004d
INUK-ai <arheon2469@gmail.com>
2024-10-10 21:50:24 +0900

    Refactor: camera.js - uploadPhoto

--- src/api/camera.js
+++ src/api/camera.js
@@ -1,4 +1,4 @@
-import axiosInstance from "../axios/AxiosInstance";
+import { PostAxiosInstance } from "../axios/AxiosMethod";
 
 // 사진을 서버에 업로드하는 함수
 export const uploadPhoto = async (file) => {
@@ -12,26 +12,15 @@ export const uploadPhoto = async (file) => {
 
   try {
     // PostAxiosInstance를 사용하여 파일 업로드 요청
-    const response = await axiosInstance.post('/upload', formData, {
+    const response = await PostAxiosInstance('/upload', formData, {
       headers: {
         'Content-Type': 'multipart/form-data', // 파일 업로드를 위한 헤더
       },
     });
 
-    // 서버에서 새 토큰을 응답하면 로컬 스토리지에 저장
-    const newToken = response.headers['Authorization'];
-    const newRefresh = response.headers['Refresh-Token'];
-
-    if (newToken) {
-      localStorage.setItem('token', newToken.split(' ')[1]);
-    }
-
-    if (newRefresh) {
-      localStorage.setItem('refresh', newRefresh.split(' ')[1]);
-    }
-
     // 업로드 성공 시 반환할 데이터
     return response.data;
+
   } catch (error) {
     console.error('Error uploading the photo:', error);
     throw new Error('Failed to upload the photo.');
