4b8b42372724175d5699ec8cd13a19a7fa75400c
INUK-ai <arheon2469@gmail.com>
2024-10-16 09:42:54 +0900

    Refactor: uploadPhoto - email parameter 추가

--- src/api/camera.js
+++ src/api/camera.js
@@ -1,7 +1,8 @@
 import { PostAxiosInstance } from "../axios/AxiosMethod";
 
 // 사진을 서버에 업로드하는 함수
-export const uploadPhoto = async (file) => {
+export const uploadPhoto = async (file, email) => {
+  
   if (!file) {
     throw new Error('No file provided for upload.');
   }
@@ -9,14 +10,11 @@ export const uploadPhoto = async (file) => {
   // FormData 객체 생성 및 파일 추가
   const formData = new FormData();
   formData.append('image', file); // 서버에 'image' 필드로 파일 전송
+  formData.append('email', email);
 
   try {
     // PostAxiosInstance를 사용하여 파일 업로드 요청
-    const response = await PostAxiosInstance('/file', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data', // 파일 업로드를 위한 헤더
-      },
-    });
+    const response = await PostAxiosInstance('/file', formData);
 
     // 업로드 성공 시 반환할 데이터
     return response.data;
@@ -26,3 +24,4 @@ export const uploadPhoto = async (file) => {
     throw new Error('Failed to upload the photo.');
   }
 };
+
