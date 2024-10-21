d2eac268a3fa15cfd9c3bc804258d3593e71faee
INUK-ai <arheon2469@gmail.com>
2024-10-21 14:51:51 +0900

    Feat: MediaPipe

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,9 +8,5 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 7979,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.pem'))
-    },
   },
 })
